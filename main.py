135ede43333c8b03cf7b300cdb1008f191cfff30
insightpal <148829263+insightpal@users.noreply.github.com>
2023-10-24 18:16:30 +0000

    added more data viz

--- main.py
+++ main.py
@@ -18,38 +18,63 @@ sales_df.to_csv('sales_data.csv', index=False)
 
 import streamlit as st
 import pandas as pd
+import numpy as np
+import altair as alt
 
 # Load the fake sales dataset
 sales_df = pd.read_csv('sales_data.csv')
 
-# Create multiple tabs
+# Add tabs to the Streamlit app
 st.title("E-commerce Sales Analysis")
 st.subheader("Explore Sales Data")
 
-# Add tabs to the Streamlit app
-tabs = ["Overview", "Product Analysis", "Date Analysis"]
-selected_tab = st.radio("Select a Tab:", tabs)
+# Create multiple tabs using st.submenu
+tabs = st.radio("Select a Tab:", ("Overview", "Product Analysis", "Date Analysis", "Data Visualization"))
 
-if selected_tab == "Overview":
+if tabs == "Overview":
     st.write("## Sales Overview")
-    st.write(sales_df)
+    st.dataframe(sales_df)
 
-elif selected_tab == "Product Analysis":
+elif tabs == "Product Analysis":
     st.write("## Product Analysis")
     selected_product = st.selectbox("Select a Product:", sales_df['Product'].unique())
 
     product_df = sales_df[sales_df['Product'] == selected_product]
-    st.write(product_df)
 
     # Add filters and radio buttons
     st.write("### Filter Data")
     min_quantity = st.slider("Minimum Quantity Sold", min_value=1, max_value=100, value=1)
     filtered_product_df = product_df[product_df['Quantity Sold'] >= min_quantity]
-    st.write(filtered_product_df)
+    st.dataframe(filtered_product_df)
 
-elif selected_tab == "Date Analysis":
+elif tabs == "Date Analysis":
     st.write("## Date Analysis")
     st.line_chart(sales_df.groupby('Date')['Revenue'].sum())
 
-# You can add more interactions and features based on your requirements
+elif tabs == "Data Visualization":
+    st.write("## Data Visualization")
+
+    # Interactive Scatter Plot
+    st.write("### Interactive Scatter Plot")
+    selected_metric = st.selectbox("Select a Metric:", ('Quantity Sold', 'Revenue'))
+    c = alt.Chart(sales_df).mark_circle().encode(
+        x='Date',
+        y=selected_metric,
+        color='Product',
+        size='Quantity Sold'
+    ).interactive()
+    st.altair_chart(c, use_container_width=True)
+
+    # Distribution Plot
+    st.write("### Distribution Plot")
+    selected_feature = st.selectbox("Select a Feature:", ('Quantity Sold', 'Revenue'))
+    hist_values = np.histogram(sales_df[selected_feature], bins=20)[0]
+    st.bar_chart(hist_values)
+
+    # Advanced filters
+    st.write("### Advanced Filters")
+    min_revenue = st.number_input("Minimum Revenue", value=0, min_value=0, max_value=int(sales_df['Revenue'].max()))
+    filtered_data = sales_df[sales_df['Revenue'] >= min_revenue]
+    st.dataframe(filtered_data)
+
 
